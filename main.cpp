0646515c5bb9426721098e656158ecc172e87760
Mehedi Hasan Shibli <23-51036-1@student.aiub.edu>
2024-06-27 08:12:27 +0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  democpp1
+//
+//  Created by MEHEDI HASAN SHIBLI on 6/6/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
